57a08dbf9b193e9f4aae95fba933c38df2ddb004
Vince Loable <vincejaphethloable@gmail.com>
2018-09-03 18:40:27 +0800

    Try Print

--- agri.py
+++ agri.py
@@ -1,3 +1,25 @@
 import os
 import psycopg2
 DB_URL = os.environ['DATABASE_URL']
+
+connection = psycopg2.connect(DATABASE_URL, sslmode='require')
+cursor = conn.cursor()
+
+print("Test")
+
+"""class Coordinates:
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+		
+class User:
+	def __init__(self, name, coords, desc):
+		self.arg = arg
+
+	def create(self):
+		pass
+
+	@property
+	def commit(self):
+		
+"""
